eeada1368301ec080b671a36a40ea1e3dd8f138b
kiran <kletherwala@gmail.com>
2021-12-03 15:17:54 -0600

    few changes. done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -34,22 +34,5 @@ test("shows success message on submit with form details", () => {
     userEvent.click(button);
     
     const successMessage = screen.getByTestId('successMessage');
-    // eslint-disable-next-line no-unused-expressions
     expect(successMessage).toBeInTheDocument;
-    // const success = screen.getByTestId('successMessage');
-    // expect(success).toBeInTheDocument();
-    // expect(screen.getByTestId("successMessage")).toBeInTheDocument();
-
-    // await waitFor(()=>{
-    //     const message = screen.queryByText(/You have ordered some plants! Woo-hoo!/i)
-    //     expect(message).toBeInTheDocument();
-
-    //     expect(screen.getByTestId('successMessage')).toHaveTextContent('kiran');
-    //     expect(screen.getByTestId('successMessage')).toHaveTextContent('letherwala');
-    //     expect(screen.getByTestId('successMessage')).toHaveTextContent('abc blvd');
-    //     expect(screen.getByTestId('successMessage')).toHaveTextContent('houston');
-    //     expect(screen.getByTestId('successMessage')).toHaveTextContent('texas');
-    //     expect(screen.getByTestId('successMessage')).toHaveTextContent('12345');
-
-    // })
 });
